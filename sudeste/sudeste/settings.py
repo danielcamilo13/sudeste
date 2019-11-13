8ad8509ed697f7e525c1ba04583393441c337df2
Daniel Camilo <daniel.camilo13@outlook.com>
2019-11-13 13:38:41 -0300

    2019 11 13 13:38

--- sudeste/sudeste/settings.py
+++ sudeste/sudeste/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/www/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATICFILES_DIRS=[
     '/www/static',
 ]
