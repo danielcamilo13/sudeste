e0aa639a8eb401eab1bef9f8edb63bc7457db149
Daniel Camilo <daniel.camilo13@outlook.com>
2019-11-13 16:51:26 -0300

    2019 11 13 16:51

--- sudeste/sudeste/settings.py
+++ sudeste/sudeste/settings.py
@@ -134,7 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/www/static/'
+# STATIC_ROOT = '/www/static'
+STATICFILES_DIRS = ['www/static',]
+
+# STATIC_ROOT = os.path.join('www','static')
 #A linha abaixo funcionou com sucesso no AWS porem localmente os estaticos não são carregados
 # STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
