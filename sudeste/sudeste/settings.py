79ff682263a74c143e0ab380b5bf5a2af3c79b9c
Daniel Camilo <daniel.camilo13@outlook.com>
2019-11-14 03:25:46 -0300

    outro

--- sudeste/sudeste/settings.py
+++ sudeste/sudeste/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'sudeste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'..','www','static')
-STATIC_URL = '/www/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'www')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'www','static'),]
 
