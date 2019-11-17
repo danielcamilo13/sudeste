4cff4003ab3934974dc789b38e58bdb99f202a74
Daniel Camilo <daniel.camilo13@outlook.com>
2019-11-17 13:50:07 -0300

    2019 11 17 13:50

--- sudeste/sudeste/settings.py
+++ sudeste/sudeste/settings.py
@@ -134,8 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'..','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
+#quando for executado localmente habilitar trecho abaixo
 # STATICFILES_DIRS = [
 #                     'static', os.path.join(BASE_DIR, 'sudeste','static'),
 #                     'medias',os.path.join(BASE_DIR,'static','medias'),
