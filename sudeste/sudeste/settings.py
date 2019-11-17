d212fb93315079a89eb43c0d7e4abfc701969eb7
Daniel Camilo <daniel.camilo13@outlook.com>
2019-11-17 00:49:38 -0300

    2019 11 17 12:49 - 3 primeiros cadastros

--- sudeste/sudeste/settings.py
+++ sudeste/sudeste/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'..','static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-                    'static', os.path.join(BASE_DIR, 'static'),
+                    # 'static', os.path.join(BASE_DIR, 'static'),
                     'medias',os.path.join(BASE_DIR,'static','medias'),
                     ]
 MEDIA_URL='/media/'
