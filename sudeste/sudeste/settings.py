f5749dca3d147bcd9002048f88efa2e2fab13280
Daniel Camilo <daniel.camilo13@outlook.com>
2019-11-17 13:04:54 -0300

    2019 11 17 13:04

--- sudeste/sudeste/settings.py
+++ sudeste/sudeste/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR,'..','static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-                    'static', os.path.join(BASE_DIR, '..','static'),
-                    'medias',os.path.join(BASE_DIR,'..','static','medias'),
+                    'static', os.path.join(BASE_DIR, 'sudeste','static'),
+                    'medias',os.path.join(BASE_DIR,'sudeste','static','medias'),
                     ]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
